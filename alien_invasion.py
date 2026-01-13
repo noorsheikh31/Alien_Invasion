869a9d7f595e6d78e1bdb3bc847fbc81128546fd
noorsheikh31 <noorsheikh0031@gmail.com>
2026-01-13 09:35:28 +0100

    Add files via upload

---
+++ alien_invasion.py
@@ -0,0 +1,190 @@
+#!/usr/bin/python3
+import sys
+import pygame
+from scoreboard import Scoreboard
+from ship import Ship
+from time import sleep
+from game_stats import GameStats
+from bullet import Bullet
+from alien import Alien
+from button import Button
+from settings import Settings
+class AlienInvasion:
+	def __init__(self):
+		pygame.init()
+		self.settings=Settings()
+		self.clock=pygame.time.Clock()
+		self.screen=pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+		self.settings.screen_width=self.screen.get_rect().width
+		self.settings.screen_height=self.screen.get_rect().height
+		pygame.display.set_caption("Alien Invasion")
+		self.ship=Ship(self)
+		self.bullets=pygame.sprite.Group()
+		self.aliens=pygame.sprite.Group()
+		self._create_fleet()
+		self.stats=GameStats(self)
+		self.game_active=False
+		self.play_button=Button(self,"Play")
+		self.stats=GameStats(self)
+		self.sb=Scoreboard(self)
+#	def run_game(self):
+#		while True:
+#			for event in pygame.event.get():
+#				print('Inside loop')
+#				if event.type == pygame.QUIT :
+#					print('Inside event type')
+#					sys.exit()
+#			self.screen.fill(self.settings.bg_color)
+#			self.ship.blitme()
+#			pygame.display.flip()
+#			self.clock.tick(60)
+	def run_game(self):
+		while True:
+			self._check_events()
+			if self.game_active :
+				self.ship.update()
+				self._update_bullets()
+				self._update_aliens()
+			
+			self._update_screen()
+			self.clock.tick(60)
+	def _check_events(self):
+		for event in pygame.event.get():
+			if event.type==pygame.QUIT:
+				sys.exit()
+			elif event.type==pygame.KEYDOWN:
+				self._check_keydown_events(event)
+#				if event.key==pygame.K_RIGHT:
+#					self.ship.moving_right=True
+#				elif event.key==pygame.K_LEFT:
+#					self.ship.moving_left=True
+			elif event.type==pygame.KEYUP:
+			#	if event.key==pygame.K_RIGHT:
+			#		self.ship.moving_right=False
+			#	elif event.key==pygame.K_LEFT:
+			#		self.ship.moving_left=False
+				self._check_keyup_events(event)
+			elif event.type==pygame.MOUSEBUTTONDOWN:
+				mouse_pos=pygame.mouse.get_pos()
+				self._check_play_button(mouse_pos)
+	def _check_play_button(self,mouse_pos):
+		button_clicked=self.play_button.rect.collidepoint(mouse_pos)
+		if button_clicked and not self.game_active:
+			self.settings.initialize_dynamic_settings()
+			self.stats.reset_stats()
+			self.sb.prep_score()
+			self.sb.prep_level()
+			self.game_active=True
+			self.bullets.empty()
+			self.aliens.empty()
+			self._create_fleet()
+			self.ship.center_ship()
+			pygame.mouse.set_visible(False)
+	def _check_keydown_events(self,event):
+		if event.key==pygame.K_RIGHT:
+			self.ship.moving_right=True
+		elif event.key==pygame.K_LEFT:
+			self.ship.moving_left=True
+		elif event.key==pygame.K_q:
+			sys.exit()
+		elif event.key==pygame.K_SPACE:
+			self._fire_bullet()
+	def _fire_bullet(self):
+		if len(self.bullets) < self.settings.bullets_allowed:
+			new_bullet=Bullet(self)
+			self.bullets.add(new_bullet)
+	def _check_keyup_events(self,event):
+		if event.key==pygame.K_RIGHT:
+			self.ship.moving_right=False
+		elif event.key==pygame.K_LEFT:
+			self.ship.moving_left=False
+	def _update_screen(self):
+		self.screen.fill(self.settings.bg_color)
+		for bullet in self.bullets.sprites():
+			bullet.draw_bullet()
+		self.ship.blitme()
+		self.aliens.draw(self.screen)
+		if not self.game_active:
+			self.play_button.draw_button()
+		self.sb.show_score()
+		pygame.display.flip()
+	def _update_bullets(self):
+		self.bullets.update()
+		for bullet in self.bullets.copy():
+			if bullet.rect.bottom<=0:
+				self.bullets.remove(bullet)
+		self._check_bullet_alien_collisions()
+	#	collisions=pygame.sprite.groupcollide(self.bullets,self.aliens,True,True)
+	#	if not self.aliens:
+	#		self.bullets.empty()
+	#		self._create_fleet()
+	def _check_bullet_alien_collisions(self):
+		collisions=pygame.sprite.groupcollide(self.bullets,self.aliens,True,True)
+		if not self.aliens:
+			self.bullets.empty()
+			self._create_fleet()
+			self.settings.increase_speed()
+			self.stats.level+=1
+			self.sb.prep_level()
+		if collisions:
+			self.stats.score+=self.settings.alien_points
+			self.sb.prep_score()
+			self.sb.check_high_score()
+	def _create_fleet(self):
+		alien=Alien(self)
+		alien_width,alien_height=alien.rect.size
+		current_x,current_y=alien_width,alien_height
+		while current_y <(self.settings.screen_height-3*alien_width):
+			while current_x<(self.settings.screen_width-2*alien_width):
+				#new_alien=Alien(self)
+				#new_alien.x=current_x
+				#new_alien.rect.x=current_x
+				#self.aliens.add(new_alien)
+				self._create_alien(current_x,current_y)
+				current_x+=2*alien_width
+			current_x=alien_width
+			current_y+=2*alien_height
+	def _create_alien(self,x_position,y_position):
+		new_alien=Alien(self)
+		new_alien.x=x_position
+		new_alien.rect.x=x_position
+		new_alien.rect.y=y_position
+		self.aliens.add(new_alien)
+	def _update_aliens(self):
+		self.aliens.update()
+		if pygame.sprite.spritecollideany(self.ship,self.aliens):
+			self._ship_hit()
+		self._check_aliens_bottom()
+		self._check_fleet_edges()
+		self.aliens.update()
+	def _check_fleet_edges(self):
+		for alien in self.aliens.sprites():
+			if alien.check_edges():
+				self._change_fleet_direction()
+				break
+	def _change_fleet_direction(self):
+		for alien in self.aliens.sprites():
+			alien.rect.y+=self.settings.fleet_drop_speed
+		self.settings.fleet_direction *=-1
+	
+#		self.aliens.update()
+	def _ship_hit(self):
+		if self.stats.ships_left>0:
+			self.stats.ships_left-=1
+			self.bullets.empty()
+			self.aliens.empty()
+			self._create_fleet()
+			self.ship.center_ship()
+			sleep(0.5)
+		else:
+			self.game_active=False
+			pygame.mouse.set_visible(True)
+	def _check_aliens_bottom(self):
+		for alien in self.aliens.sprites():
+			if alien.rect.bottom>=self.settings.screen_height:
+				self._ship_hit()
+				break
+if __name__=='__main__':
+	ai=AlienInvasion()
+	ai.run_game()
+                       
